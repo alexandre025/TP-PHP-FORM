27b84c8866a9a5c8771c56a786e5001e88f2be52
Alexandre Ferraille <alexandre.ferraille@gmail.com>
2020-03-24 10:31:24 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<?php include('views/head.php'); ?>
+<body>
+<?php include('views/header.php'); ?>
+
+<div class="container">
+    <h2>Contact</h2>
+    <div class="row mt-4">
+        <form action="controllers/contact_form.php" method="POST" class="col-lg">
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="foo@bar.net">
+            </div>
+            <div class="form-group">
+                <label for="first_name">Full name or company</label>
+                <input type="text" class="form-control" name="full_name_or_company" placeholder="John Doe">
+            </div>
+
+            <div class="form-group">
+                <label for="comment">Comment</label>
+                <textarea class="form-control" name="comment" rows="10"></textarea>
+            </div>
+            <hr>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</div>
+</body>
+</html>
